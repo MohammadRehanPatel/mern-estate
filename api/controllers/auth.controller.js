d2967a61ddecb116f4a3768c7c34549ceb9ecaf8
MohammadRehanPatel <en20ca301008@medicaps.ac.in>
2023-10-02 18:05:21 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,5 +12,29 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User Created Succesfully");
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid Credentails!"));
+    }
+    const isMatch = bcryptjs.compareSync(password, validUser.password);
+    if (!isMatch) {
+      return next(errorHandler(401, "Invalid Credentails!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      // expiresIn: "1d",
+    });
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
 };
