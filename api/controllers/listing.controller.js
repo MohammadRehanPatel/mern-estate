24736211e8f4c414c9c8f01fab139b097946bbbb
MohammadRehanPatel <en20ca301008@medicaps.ac.in>
2023-10-25 17:32:00 +0530

    create updae listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -21,5 +22,25 @@ export const deleteListing = async (req, res, next) => {
     res.status(200).json({ message: "Listing deleted..." });
   } catch (error) {
     next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only edit your own listings"));
+  }
+  try {
+    let updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (e) {
+    next(e);
   }
 };
