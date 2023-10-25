f9801de3841b5ce7c7536abe18474dc5f6af7666
MohammadRehanPatel <en20ca301008@medicaps.ac.in>
2023-10-25 17:20:03 +0530

    complete delete user listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -51,13 +51,14 @@ export const deleteUser = async (req, res, next) => {
 };
 
 export const getUserListings = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can only get your own listings"));
-  }
-  try {
-    const listing = await Listing.findById(req.params.id);
-    res.status(200).json(listing);
-  } catch (error) {
-    next(error);
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
