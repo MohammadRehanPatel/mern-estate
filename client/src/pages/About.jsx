e408bb5a7deb88c97916a585a49df6adf406a2c3
MohammadRehanPatel <en20ca301008@medicaps.ac.in>
2023-11-03 17:50:14 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-blue-900 text-center'>About It's Home Estate</h1>
+      <p className='mb-4 text-blue-950 font-semibold'>
+        This is a website about the home. The purpose of this site is to provide information and resources for people who are looking for their dream house or 
+
+        It's Home Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.
+      </p>
+      <p className='mb-4 text-blue-950 font-semibold'>
+      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-blue-950 font-semibold'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
   )
 }
 
